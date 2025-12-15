9873df238ec4d56031ea1191d142144d30be34d5
realfakenerd <lucas210898@hotmail.com>
2025-12-15 14:26:47 -0300

    feat: Implement ColumnBuilder and Column classes
    Introduces base classes for defining and configuring DynamoDB columns,
    including type safety and runtime configuration options.

---
+++ src/column-builder.ts
@@ -0,0 +1,293 @@
+import { type Column, ExtraConfigColumn } from "./column";
+import type { AnyTable } from "./table";
+import type { Assume, Simplify } from "./utils";
+
+export type ColumnDataType =
+    | "S"
+    | "N"
+    | "BOOL"
+    | "M"
+    | "L"
+    | "B"
+    | "BS"
+    | "NS"
+    | "NULL"
+    | "SS";
+
+export interface ColumnBuilderBaseConfig<
+    TDataType extends string,
+    TColumnDataType extends ColumnDataType,
+> {
+    name: string;
+    dataType: TDataType;
+    columnType: TColumnDataType;
+    data: unknown;
+}
+
+export type BuildExtraConfigColumns<
+    _TTableName extends string,
+    TConfigMap extends Record<string, ColumnBuilderBase>,
+> = { [Key in keyof TConfigMap]: ExtraConfigColumn } & {};
+
+export type MakeColumnConfig<
+    T extends ColumnBuilderBaseConfig<string, ColumnDataType>,
+    TTableName extends string,
+    TData = T extends { $type: infer U } ? U : T["data"],
+> = {
+    name: T["name"];
+    tableName: TTableName;
+    dataType: T["dataType"];
+    columnType: T["columnType"];
+    data: TData;
+    notNull: T extends { notNull: true } ? true : false;
+    hasDefault: T extends { hasDefault: true } ? true : false;
+    hasRuntimeDefault: T extends { hasRuntimeDefault: true } ? true : false;
+    isPartitionKey: T extends { isPartitionKey: true } ? true : false;
+    isSortKey: T extends { isSortKey: true } ? true : false;
+} & {};
+
+export type ColumnBuilderRuntimeConfig<
+    TData,
+    TRuntimeConfig extends object = object,
+> = {
+    name: string;
+    notNull: boolean;
+    default: TData;
+    dataType: string;
+    columnType: string;
+    isPartitionKey: boolean;
+    isSortKey: boolean;
+    hasDefault: boolean;
+    partitionKey: boolean;
+    sortKey: boolean;
+    defaultFn: () => TData | undefined;
+    onUpdateFn: () => TData | undefined;
+} & TRuntimeConfig;
+
+export type ColumnBuilderTypeConfig<
+    T extends ColumnBuilderBaseConfig<string, ColumnDataType>,
+    TTypeConfig extends object = object,
+> = Simplify<
+    {
+        name: T["name"];
+        dataType: T["dataType"];
+        data: T["data"];
+        columnType: T["columnType"];
+        notNull: T extends { notNull: infer U } ? U : boolean;
+        hasDefault: T extends { hasDefault: infer U } ? U : boolean;
+        isPartitionKey: T extends { isPartitionKey: infer U } ? U : boolean;
+        isSortKey: T extends { isSortKey: infer U } ? U : boolean;
+    } & TTypeConfig
+>;
+
+export interface ColumnBuilderBase<
+    T extends ColumnBuilderBaseConfig<
+        string,
+        ColumnDataType
+    > = ColumnBuilderBaseConfig<string, ColumnDataType>,
+    TTypeConfig extends object = object,
+> {
+    _: ColumnBuilderTypeConfig<T, TTypeConfig>;
+}
+
+export interface ColumnBuiderExtraConfig {
+    partitionKeyHasDefault?: boolean;
+}
+
+export type NotNull<T extends ColumnBuilderBase> = T & {
+    _: {
+        notNull: true;
+    };
+};
+
+export type HasDefault<T extends ColumnBuilderBase> = T & {
+    _: {
+        hasDefault: true;
+    };
+};
+
+export type HasRuntimeDefault<T extends ColumnBuilderBase> = T & {
+    _: {
+        hasRuntimeDefault: true;
+    };
+};
+
+export type IsPartitionKey<T extends ColumnBuilderBase> = T & {
+    _: {
+        isPartitionKey: true;
+        notNull: false;
+    };
+};
+
+export type IsSortKey<T extends ColumnBuilderBase> = T & {
+    _: {
+        isSortKey: true;
+        notNull: false;
+    };
+};
+
+export type $Type<T extends ColumnBuilderBase, TType> = T & {
+    _: { $type: TType };
+};
+
+export type BuildColumn<
+    TTableName extends string,
+    TBuilder extends ColumnBuilderBase,
+> = Column<
+    MakeColumnConfig<TBuilder["_"], TTableName>,
+    {},
+    Simplify<
+        Omit<TBuilder["_"], keyof MakeColumnConfig<TBuilder["_"], TTableName>>
+    >
+>;
+
+export type BuildColumns<
+    TTableName extends string,
+    TConfigMap extends Record<string, ColumnBuilderBase>,
+> = {
+    [Key in keyof TConfigMap]: BuildColumn<
+        TTableName,
+        {
+            _: Omit<TConfigMap[Key]["_"], "name"> & {
+                name: TConfigMap[Key]["_"]["name"] extends ""
+                    ? Assume<Key, string>
+                    : TConfigMap[Key]["_"]["name"];
+            };
+        }
+    >;
+} & {};
+
+export abstract class ColumnBuider<
+    T extends ColumnBuilderBaseConfig<
+        string,
+        ColumnDataType
+    > = ColumnBuilderBaseConfig<string, ColumnDataType>,
+    TRuntimeConfig extends object = object,
+    TTypeConfig extends object = object,
+    TExtraConfig extends ColumnBuiderExtraConfig = ColumnBuiderExtraConfig,
+> implements ColumnBuilderBase<T, TTypeConfig>
+{
+    declare _: ColumnBuilderTypeConfig<T, TTypeConfig>;
+    protected config: ColumnBuilderRuntimeConfig<T["data"], TRuntimeConfig>;
+
+    constructor(
+        name: T["name"],
+        dataType: T["dataType"],
+        columnType: T["columnType"],
+    ) {
+        this.config = {
+            name,
+            dataType,
+            columnType,
+            notNull: false,
+            default: undefined,
+            hasDefault: false,
+            isPartitionKey: false,
+            isSortKey: false,
+        } as ColumnBuilderRuntimeConfig<T["data"], TRuntimeConfig>;
+    }
+
+    default(
+        value: this["_"] extends { $type: infer U } ? U : this["_"]["data"],
+    ): HasDefault<this> {
+        this.config.default = value;
+        this.config.hasDefault = true;
+        return this as HasDefault<this>;
+    }
+
+    $defaultFn(
+        fn: () => this["_"] extends { $type: infer U } ? U : this["_"]["data"],
+    ): HasRuntimeDefault<HasDefault<this>> {
+        this.config.defaultFn = fn;
+        this.config.hasDefault = true;
+        return this as HasRuntimeDefault<HasDefault<this>>;
+    }
+
+    $onUpdateFn(
+        fn: () => this["_"] extends { $type: infer U } ? U : this["_"]["data"],
+    ): HasDefault<this> {
+        this.config.onUpdateFn = fn;
+        this.config.hasDefault = true;
+        return this as HasDefault<this>;
+    }
+
+    /**
+     * Change de data type of the column.
+     *
+     * ```ts
+     * const users = dynamoTable('users', {
+     *  details: map().$type<UserDetails>().nullable()
+     * });
+     * ```
+     */
+    $type<TType>(): $Type<this, TType> {
+        return this as $Type<this, TType>;
+    }
+
+    /** @internal */
+    getConfig() {
+        return this.config;
+    }
+
+    partitionKey(): TExtraConfig["partitionKeyHasDefault"] extends true
+        ? IsPartitionKey<HasDefault<NotNull<this>>>
+        : IsPartitionKey<NotNull<this>> {
+        this.config.isPartitionKey = true;
+        this.config.notNull = false;
+        return this as TExtraConfig["partitionKeyHasDefault"] extends true
+            ? IsPartitionKey<HasDefault<NotNull<this>>>
+            : IsPartitionKey<NotNull<this>>;
+    }
+
+    sortKey(): IsSortKey<this> {
+        this.config.isSortKey = true;
+        this.config.notNull = false;
+        return this as IsSortKey<this>;
+    }
+
+    /**
+     * Turns a column nullable.
+     *
+     * ```ts
+     * const users = dynamoTable('users', {
+     *  name: string().nullable()
+     * });
+     * ```
+     */
+    notNull(): NotNull<this> {
+        this.config.notNull = true;
+        return this as NotNull<this>;
+    }
+
+    /** @internal */
+    setName(name: string) {
+        if (this.config.name !== "") return;
+        this.config.name = name;
+    }
+
+    /** @internal */
+    abstract build<TTableName extends string>(
+        table: AnyTable<{ name: TTableName }>,
+    ): Column<MakeColumnConfig<T, TTableName>>;
+
+    buildExtraConfigColumn<TTableName extends string>(
+        table: AnyTable<{ name: TTableName }>,
+    ): ExtraConfigColumn {
+        return new ExtraConfigColumn(table, this.config);
+    }
+
+    /** @internal */
+    buildSortKey(column: Column): LsiBuilder[] {
+        const { isSortKey, name } = this.config;
+        if (!isSortKey) {
+            return [];
+        }
+
+        return [
+            lsi({
+                name: `${column.table[TableName]!}-${name}-lsi`,
+                sk: column,
+            }),
+        ];
+    }
+}
---
+++ src/column.ts
@@ -0,0 +1,162 @@
+import type {
+    ColumnBuilderBaseConfig,
+    ColumnBuilderRuntimeConfig,
+    ColumnDataType,
+} from "./column-builder";
+import type { Table } from "./table";
+import type { Update } from "./utils";
+
+export type AnyColumn<
+    TPartial extends Partial<ColumnBaseConfig<string, ColumnDataType>> = {},
+> = Column<
+    Required<Update<ColumnBaseConfig<string, ColumnDataType>, TPartial>>
+>;
+
+export interface ColumnBaseConfig<
+    TDataType extends string,
+    TColumnType extends ColumnDataType,
+> extends ColumnBuilderBaseConfig<TDataType, TColumnType> {
+    tableName: string;
+    name: string;
+    notNull: boolean;
+    hasDefault: boolean;
+    hasRuntimeDefault: boolean;
+    isPartitionKey: boolean;
+    isSortKey: boolean;
+    validators?: Record<string, any>;
+}
+
+export type ColumnTypeConfig<
+    T extends ColumnBaseConfig<string, ColumnDataType>,
+    TTypeConfig extends object,
+> = T & {
+    tableName: T["tableName"];
+    name: T["name"];
+    dataType: T["dataType"];
+    columnType: T["columnType"];
+    data: T["data"];
+    notNull: T["notNull"];
+    hasDefault: T["hasDefault"];
+    hasRuntimeDefault: T["hasRuntimeDefault"];
+    isPartitionKey: T["isPartitionKey"];
+    isSortKey: T["isSortKey"];
+} & TTypeConfig;
+
+export type ColumnRuntimeConfig<
+    TData,
+    TRuntimeConfig extends object,
+> = ColumnBuilderRuntimeConfig<TData, TRuntimeConfig>;
+
+export abstract class Column<
+    T extends ColumnBaseConfig<string, ColumnDataType> = ColumnBaseConfig<
+        string,
+        ColumnDataType
+    >,
+    TRuntimeConfig extends object = object,
+    TTypeConfig extends object = object,
+> {
+    declare readonly _: ColumnTypeConfig<T, TTypeConfig>;
+
+    readonly name: string;
+    readonly primary: boolean;
+    readonly notNull: boolean;
+    readonly default: T["data"] | undefined;
+    readonly defaultFn: () => T["data"] | undefined;
+    readonly hasDefault: boolean;
+    readonly dataType: T["dataType"];
+    readonly columnType: T["columnType"];
+
+    protected config: ColumnRuntimeConfig<T["data"], TRuntimeConfig>;
+
+    constructor(
+        readonly table: Table,
+        config: ColumnRuntimeConfig<T["data"], TRuntimeConfig>,
+    ) {
+        this.config = config;
+        this.name = config.name;
+        this.primary = config.partitionKey;
+        this.notNull = config.notNull;
+        this.default = config.default;
+        this.defaultFn = config.defaultFn;
+        this.hasDefault = config.hasDefault;
+        this.dataType = config.dataType;
+        this.columnType = config.columnType as T["columnType"];
+    }
+
+    abstract getDynamoType(): string;
+
+    mapFromDynamoValue(value: unknown): unknown {
+        return value;
+    }
+
+    mapToDynamoValue(value: unknown): unknown {
+        return value;
+    }
+}
+
+export type GetColumnData<
+    TColumn extends Column,
+    TInferMode extends "query" | "raw" = "query",
+> = TInferMode extends "raw"
+    ? TColumn["_"]["data"]
+    : TColumn["_"]["notNull"] extends false
+      ? TColumn["_"]["data"]
+      : TColumn["_"]["data"] | null;
+
+export type InferColumndsDataTypes<TColumns extends Record<string, Column>> = {
+    [Key in keyof TColumns]: GetColumnData<TColumns[Key]>;
+};
+
+export type IndexedExtraConfigType = {
+    order?: "asc" | "desc";
+    nulls?: "first" | "last";
+    opClass?: string;
+};
+
+export class ExtraConfigColumn<
+    T extends ColumnBaseConfig<string, ColumnDataType> = ColumnBaseConfig<
+        string,
+        ColumnDataType
+    >,
+> extends Column<T, IndexedExtraConfigType> {
+    override getDynamoType(): string {
+        return this.getDynamoType();
+    }
+
+    indexConfig: IndexedExtraConfigType = {
+        order: this.config.order ?? "asc",
+        nulls: this.config.nulls ?? "last",
+        opClass: this.config.opClass,
+    };
+
+    defaultConfig: IndexedExtraConfigType = {
+        order: "asc",
+        nulls: "last",
+        opClass: undefined,
+    };
+
+    asc(): Omit<this, "asc" | "desc"> {
+        this.indexConfig.order = "asc";
+        return this;
+    }
+
+    desc(): Omit<this, "asc" | "desc"> {
+        this.indexConfig.order = "desc";
+        return this;
+    }
+
+    nullsFirst(): Omit<this, "nullsFirst" | "nullsLast"> {
+        this.indexConfig.nulls = "first";
+        return this;
+    }
+
+    nullsLast(): Omit<this, "nullsFirst" | "nullsLast"> {
+        this.indexConfig.nulls = "last";
+        return this;
+    }
+
+    op(opClass: string): Omit<this, "opClass"> {
+        this.indexConfig.opClass = opClass;
+        return this;
+    }
+}
