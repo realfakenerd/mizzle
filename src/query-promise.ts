d583ff723192abe65563ab1c9d5c362636de51b9
realfakenerd <lucas210898@hotmail.com>
2025-12-15 15:51:52 -0300

    Add QueryPromise abstract class
    Introduce an abstract class QueryPromise that implements the Promise
    interface. This class provides default implementations for `catch` and
    `finally`, deferring the core promise execution to an abstract `execute`
    method that subclasses must implement.

---
+++ src/query-promise.ts
@@ -0,0 +1,41 @@
+export abstract class QueryPromise<T> implements Promise<T> {
+    [Symbol.toStringTag] = "QueryPromise";
+
+    catch<TResult = never>(
+        onrejected?:
+            | ((reason: any) => TResult | PromiseLike<TResult>)
+            | null
+            | undefined,
+    ): Promise<T | TResult> {
+        return this.then(undefined, onrejected);
+    }
+
+    finally(onfinally?: (() => void) | null | undefined): Promise<T> {
+        return this.then(
+            (value) => {
+                onfinally?.();
+                return value;
+            },
+            (reason) => {
+                onfinally?.();
+                throw reason;
+            },
+        );
+    }
+
+    // oxlint-disable
+    then<TResult1 = T, TResult2 = never>(
+        onfulfilled?:
+            | ((value: T) => TResult1 | PromiseLike<TResult1>)
+            | null
+            | undefined,
+        onrejected?:
+            | ((reason: any) => TResult2 | PromiseLike<TResult2>)
+            | null
+            | undefined,
+    ): Promise<TResult1 | TResult2> {
+        return this.execute().then(onfulfilled, onrejected);
+    }
+
+    abstract execute(): Promise<T>;
+}
