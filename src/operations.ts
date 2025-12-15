215e237534d124d39ba3c1e461d256cf0eafd60b
realfakenerd <lucas210898@hotmail.com>
2025-12-15 15:31:14 -0300

    Add operations types

---
+++ src/operations.ts
@@ -0,0 +1,26 @@
+import type { Column, AnyColumn } from "./column";
+import type { Table } from "./table";
+
+export type RequiredKeyOnly<TKey extends string, T extends Column> =
+    T extends AnyColumn<{
+        hasDefault: false;
+        isNull: false;
+    }>
+        ? TKey
+        : never;
+
+export type OpitionalKeyOnly<TKey extends string, T extends Column> =
+    TKey extends RequiredKeyOnly<TKey, T> ? never : TKey;
+
+export type SelectedFieldsFlat<TColumn extends Column> = Record<
+    string,
+    TColumn
+>;
+
+export type SelectedFields<
+    TColumn extends Column,
+    TTable extends Table,
+> = Record<
+    string,
+    SelectedFieldsFlat<TColumn>[string] | TTable | SelectedFieldsFlat<TColumn>
+>;
