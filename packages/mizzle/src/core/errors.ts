19d4baacaea41ec855f797959308c10762dc9363
realfakenerd <lucas210898@hotmail.com>
2026-01-09 16:46:42 -0300

    feat(tx): Implement TransactionFailedError and CancellationReason types

--- packages/mizzle/src/core/errors.ts
+++ packages/mizzle/src/core/errors.ts
@@ -10,3 +10,25 @@ export class ItemSizeExceededError extends Error {
         Object.setPrototypeOf(this, ItemSizeExceededError.prototype);
     }
 }
+
+/**
+ * Detailed reason for a transaction cancellation.
+ */
+export interface CancellationReason {
+    index: number;
+    code: string;
+    message?: string;
+    item?: Record<string, any>;
+}
+
+/**
+ * Error thrown when a DynamoDB transaction is canceled.
+ */
+export class TransactionFailedError extends Error {
+    constructor(message: string, public readonly reasons: CancellationReason[]) {
+        super(message);
+        this.name = "TransactionFailedError";
+        
+        Object.setPrototypeOf(this, TransactionFailedError.prototype);
+    }
+}
