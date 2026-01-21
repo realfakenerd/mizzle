d1357b3e284f86437c38071c51a5f5c4110ace28
realfakenerd <lucas210898@hotmail.com>
2026-01-21 18:14:19 -0300

    Update dependencies and docs configuration
    Adds several new dependencies to the project, including `astro-mermaid`,
    `mermaid`, and `starlight-llms-txt`. These likely enable new
    documentation features or integrations.
    
    Also updates the `docs/astro.config.mjs` to include these new plugins
    and configure the site's appearance with custom CSS, a favicon, and a
    logo. The `docs/package.json` is updated to include
    `starlight-links-validator`.
    
    The `docs/src/content/docs/index.mdx` is updated to use a new logo and
    tagline, and the `CardGrid` component's `stagger` prop is removed.
    
    Finally, new files for the `Button.astro` component, `Hero.astro`
    component, `colors.css`, `custom.css`, and `logo_with_name.svg` are
    added to enhance the documentation's visual presentation and
    functionality.

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,14 +1,25 @@
 // @ts-check
 import starlight from '@astrojs/starlight';
+import starlightLinksValidator from 'starlight-links-validator';
+import startlightLlmsTxt from 'starlight-llms-txt'
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mizzle-docs.vercel.app',
   integrations: [
     starlight({
-
-      title: 'Mizzle',
+      plugins: [starlightLinksValidator(), startlightLlmsTxt()],
+      title: 'mizzle',
+      components: {
+        Hero: './src/components/Hero.astro'
+      },
       description: 'a drizzle-like DynamoDB ORM',
+      customCss: ['./src/styles/custom.css', './src/styles/colors.css'],
+      favicon: 'favicon.svg',
+      logo: {
+        src: './src/assets/logo_with_name.svg'
+      },
       editLink: {
         baseUrl: 'https://github.com/realfakenerd/mizzle/edit/main/docs'
       },
