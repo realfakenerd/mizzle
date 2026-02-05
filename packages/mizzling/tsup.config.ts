56a8c26d2accea5a1e6b76d2c87b46020c3ceef3
realfakenerd <lucas210898@hotmail.com>
2026-02-05 18:06:51 -0300

    Update package manager to Bun and bump mizzle version
    Update `package.json` to use Bun as the package manager. Increment the
    version of the `@aurios/mizzle` package. Configure `mizzling` to
    generate type declaration files and adjust its `exports` field for
    better ESM compatibility. Modify `vitest-config` to enable emitting TS
    files and disable importing TS extensions.

--- packages/mizzling/tsup.config.ts
+++ packages/mizzling/tsup.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/cli.ts", "src/index.ts"],
-  format: ["esm"],
-  clean: true,
-  noExternal: ["@repo/shared", "mizzle"],
-  minify: true,
-  banner: {
-    js: "#!/usr/bin/env node",
-  },
+    entry: ["src/cli.ts", "src/index.ts"],
+    format: ["esm"],
+    clean: true,
+    noExternal: ["@repo/shared", "mizzle"],
+    minify: true,
+    banner: {
+        js: "#!/usr/bin/env node",
+    },
+    dts: true
 });
